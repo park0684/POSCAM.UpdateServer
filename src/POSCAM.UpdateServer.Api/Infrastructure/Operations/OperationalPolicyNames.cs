b2fb2abde363cd1fb79cc74ea83cd01fd92407a6
park0684 <parkjs0684@gmail.com>
2026-06-28 20:02:39 +0900

    Add operational policy names

---
+++ src/POSCAM.UpdateServer.Api/Infrastructure/Operations/OperationalPolicyNames.cs
@@ -0,0 +1,8 @@
+namespace POSCAM.UpdateServer.Api.Infrastructure.Operations;
+
+public static class OperationalPolicyNames
+{
+    public const string AdminWebCors = "AdminWebCors";
+
+    public const string UpdateCheckRateLimit = "UpdateCheckRateLimit";
+}
