a087750767710ccf4ff0930fb55d20150dc7c577
park0684 <parkjs0684@gmail.com>
2026-06-28 17:17:20 +0900

    Add release repository contract

---
+++ src/POSCAM.UpdateServer.Api/Repositories/IUpdateReleaseRepository.cs
@@ -0,0 +1,56 @@
+using System.Data;
+using POSCAM.UpdateServer.Api.Models.Domain;
+using POSCAM.UpdateServer.Api.Models.Entities;
+using POSCAM.UpdateServer.Api.Models.Queries;
+
+namespace POSCAM.UpdateServer.Api.Repositories;
+
+public interface IUpdateReleaseRepository
+{
+    Task<UpdateRelease?> GetByCodeAsync(
+        long releaseCode,
+        IDbTransaction? transaction = null,
+        CancellationToken cancellationToken = default);
+
+    Task<bool> ExistsVersionAsync(
+        string productCode,
+        string channel,
+        UpdateVersion version,
+        long? excludeReleaseCode = null,
+        IDbTransaction? transaction = null,
+        CancellationToken cancellationToken = default);
+
+    Task<long> CreateDraftAsync(
+        UpdateRelease release,
+        IDbTransaction? transaction = null,
+        CancellationToken cancellationToken = default);
+
+    Task<bool> UpdateDraftAsync(
+        UpdateRelease release,
+        IDbTransaction? transaction = null,
+        CancellationToken cancellationToken = default);
+
+    Task<bool> DeleteDraftAsync(
+        long releaseCode,
+        IDbTransaction? transaction = null,
+        CancellationToken cancellationToken = default);
+
+    Task<bool> PublishAsync(
+        long releaseCode,
+        IDbTransaction? transaction = null,
+        CancellationToken cancellationToken = default);
+
+    Task<bool> DisableAsync(
+        long releaseCode,
+        IDbTransaction? transaction = null,
+        CancellationToken cancellationToken = default);
+
+    Task<CompatibleReleaseArtifact?> FindLatestCompatibleAsync(
+        string productCode,
+        string channel,
+        string operatingSystem,
+        string requestedArchitecture,
+        string packageType,
+        IDbTransaction? transaction = null,
+        CancellationToken cancellationToken = default);
+}
