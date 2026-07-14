2b7c7d9cd1d7eaea7d5a9470c06f9011c7118589
park0684 <parkjs0684@gmail.com>
2026-07-14 09:31:04 +0900

    feat: add update apply mode constants

---
+++ src/POSCAM.UpdateClient/Models/UpdateApplyModes.cs
@@ -0,0 +1,11 @@
+namespace POSCAM.UpdateClient.Models
+{
+    /// <summary>
+    /// 적용 계획이 처리할 업데이트 유형이다.
+    /// </summary>
+    internal static class UpdateApplyModes
+    {
+        public const string FullPackage = "FullPackage";
+        public const string FileRepair = "FileRepair";
+    }
+}
