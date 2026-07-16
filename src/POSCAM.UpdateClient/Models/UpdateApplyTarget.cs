168f513a790ea4834b7d3c26bfa31acb0de2ae23
park0684 <parkjs0684@gmail.com>
2026-07-16 12:00:25 +0900

    feat: serialize incremental target operation

--- src/POSCAM.UpdateClient/Models/UpdateApplyTarget.cs
+++ src/POSCAM.UpdateClient/Models/UpdateApplyTarget.cs
@@ -3,10 +3,14 @@ using Newtonsoft.Json;
 namespace POSCAM.UpdateClient.Models
 {
     /// <summary>
-    /// 검증이 완료되어 apply 단계에서 교체할 파일 정보다.
+    /// 검증이 완료되어 apply 단계에서 교체하거나 삭제할 파일 정보다.
     /// </summary>
     internal sealed class UpdateApplyTarget
     {
+        [JsonProperty("operation")]
+        public string Operation { get; set; }
+            = UpdateTargetOperations.Replace;
+
         [JsonProperty("relativePath")]
         public string RelativePath { get; set; } = "";
 
