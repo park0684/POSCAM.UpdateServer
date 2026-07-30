704bcce4113bc6c95162bcb9b5c4a711990fa2d2
park0684 <parkjs0684@gmail.com>
2026-07-30 15:38:58 +0900

    test: cover split PC CAM product codes

--- tests/POSCAM.UpdateServer.Tests/Models/Domain/UpdateDomainCodesTests.cs
+++ tests/POSCAM.UpdateServer.Tests/Models/Domain/UpdateDomainCodesTests.cs
@@ -27,15 +27,39 @@ public class UpdateDomainCodesTests
 
     [Theory]
     [InlineData("PCCAM", true)]
+    [InlineData("PCCAM_X86", true)]
+    [InlineData("PCCAM_X64", true)]
     [InlineData("CAMVIEWER", true)]
     [InlineData("UPDATER", true)]
     [InlineData("pccam", false)]
+    [InlineData("pccam_x86", false)]
     [InlineData("UNKNOWN", false)]
     public void ProductCodes_대문자_고정코드만_허용한다(
         string productCode,
         bool expected)
     {
         Assert.Equal(expected, ProductCodes.IsSupported(productCode));
+    }
+
+    [Theory]
+    [InlineData("PCCAM", "x86", true)]
+    [InlineData("PCCAM", "x64", true)]
+    [InlineData("PCCAM_X86", "x86", true)]
+    [InlineData("PCCAM_X86", "x64", false)]
+    [InlineData("PCCAM_X64", "x64", true)]
+    [InlineData("PCCAM_X64", "x86", false)]
+    [InlineData("CAMVIEWER", "x64", true)]
+    [InlineData("UPDATER", "x86", true)]
+    [InlineData("PCCAM_X86", "any", false)]
+    [InlineData("UNKNOWN", "x86", false)]
+    public void ProductCodes_제품과_아키텍처_조합을_검증한다(
+        string productCode,
+        string architecture,
+        bool expected)
+    {
+        Assert.Equal(
+            expected,
+            ProductCodes.IsArchitectureCompatible(productCode, architecture));
     }
 
     [Theory]
